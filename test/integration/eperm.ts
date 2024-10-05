7457f3cb269466715ea6e5d566f929411e219604
Luke Karrys <luke@lukekarrys.com>
2024-10-04 17:28:58 -0700

    dont use tmpdir

--- test/integration/eperm.ts
+++ test/integration/eperm.ts
@@ -1,10 +1,8 @@
-import { mkdirSync, readdirSync, realpathSync } from 'fs'
+import t from 'tap'
+import { mkdirSync, readdirSync } from 'fs'
 import { sep, join } from 'path'
 import { globSync } from 'glob'
-import t from 'tap'
-import os from 'node:os'
 import { rimraf } from '../../src/index.js'
-import { randomBytes } from 'crypto'
 
 // Copied from sindresorhus/del since it was reported in https://github.com/isaacs/rimraf/pull/314
 // that this test would throw EPERM errors consistently in Windows CI environments. I'm not sure
@@ -14,10 +12,7 @@ t.test('does not throw EPERM - async', async t => {
   const iterations = 200
   const depth = 7
 
-  const dir = join(
-    realpathSync(os.tmpdir()),
-    `_${randomBytes(6).toString('hex')}`,
-  )
+  const dir = t.testdir()
   const nested = join(
     ...new Array(depth).fill(0).map((_, i) => (10 + i).toString(36)),
   )
@@ -28,12 +23,12 @@ t.test('does not throw EPERM - async', async t => {
   let count = 0
   while (count !== iterations) {
     mkdirSync(join(dir, nested), { recursive: true })
-    const entries = globSync('**/*', { cwd: dir, dot: true }).sort(
+    const del = globSync('**/*', { cwd: dir, dot: true }).sort(
       () => 0.5 - Math.random(),
     )
-    await Promise.all(entries.map(e => rimraf(join(dir, e), { glob: false })))
+    await Promise.all(del.map(d => rimraf(join(dir, d), { glob: false })))
     t.strictSame(
-      entries.sort((a, b) => a.localeCompare(b, 'en')),
+      del.sort((a, b) => a.localeCompare(b, 'en')),
       expected,
     )
     count += 1
