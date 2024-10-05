a233db82d27b5b24c109dbda30833471368f5ddb
Luke Karrys <luke@lukekarrys.com>
2024-10-04 17:37:54 -0700

    sort fns

--- test/integration/eperm.ts
+++ test/integration/eperm.ts
@@ -4,13 +4,16 @@ import { sep, join } from 'path'
 import { globSync } from 'glob'
 import { rimraf } from '../../src/index.js'
 
+const rando = <T>(arr: T[]): T[] => [...arr].sort(() => 0.5 - Math.random())
+const sortAlpha = (arr: string[]) =>
+  [...arr].sort((a, b) => a.localeCompare(b, 'en'))
+
 // Copied from sindresorhus/del since it was reported in https://github.com/isaacs/rimraf/pull/314
 // that this test would throw EPERM errors consistently in Windows CI environments.
 // https://github.com/sindresorhus/del/blob/chore/update-deps/test.js#L116
 t.test('does not throw EPERM - async', async t => {
   const iterations = 200
   const depth = 7
-
   const dir = t.testdir()
   const nested = join(
     ...new Array(depth).fill(0).map((_, i) => (10 + i).toString(36)),
@@ -22,17 +25,11 @@ t.test('does not throw EPERM - async', async t => {
   let count = 0
   while (count !== iterations) {
     mkdirSync(join(dir, nested), { recursive: true })
-    const del = globSync('**/*', { cwd: dir, dot: true }).sort(
-      () => 0.5 - Math.random(),
-    )
-    await Promise.all(del.map(d => rimraf(join(dir, d), { glob: false })))
-    t.strictSame(
-      del.sort((a, b) => a.localeCompare(b, 'en')),
-      expected,
-    )
+    const del = rando(globSync('**/*', { cwd: dir }))
+    await Promise.all(del.map(d => rimraf(join(dir, d))))
+    t.strictSame(sortAlpha(del), expected)
     t.strictSame(readdirSync(dir), [])
     count += 1
   }
-
   t.equal(count, iterations)
 })
