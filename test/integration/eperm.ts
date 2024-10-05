25d26c6ac555f8215a9dc8ae920f41ff44cfb791
Luke Karrys <luke@lukekarrys.com>
2024-10-04 17:26:20 -0700

    randomize array order before rimraf

--- test/integration/eperm.ts
+++ test/integration/eperm.ts
@@ -1,6 +1,6 @@
 import { mkdirSync, readdirSync, realpathSync } from 'fs'
 import { sep, join } from 'path'
-import { glob } from 'glob'
+import { globSync } from 'glob'
 import t from 'tap'
 import os from 'node:os'
 import { rimraf } from '../../src/index.js'
@@ -11,25 +11,34 @@ import { randomBytes } from 'crypto'
 // how much of the test structure is relevant to the error but I've copied it as closely as possible.
 // https://github.com/sindresorhus/del/blob/chore/update-deps/test.js#L116
 t.test('does not throw EPERM - async', async t => {
+  const iterations = 200
+  const depth = 7
+
   const dir = join(
     realpathSync(os.tmpdir()),
     `_${randomBytes(6).toString('hex')}`,
   )
-  const nested = join('a/b/c/d/e/f/g')
+  const nested = join(
+    ...new Array(depth).fill(0).map((_, i) => (10 + i).toString(36)),
+  )
   const expected = nested
     .split(sep)
     .reduce<string[]>((acc, d) => acc.concat(join(acc.at(-1) ?? '', d)), [])
-  const totalAttempts = 200
 
   let count = 0
-  while (count !== totalAttempts) {
+  while (count !== iterations) {
     mkdirSync(join(dir, nested), { recursive: true })
-    const entries = await glob('**/*', { cwd: dir, dot: true })
+    const entries = globSync('**/*', { cwd: dir, dot: true }).sort(
+      () => 0.5 - Math.random(),
+    )
     await Promise.all(entries.map(e => rimraf(join(dir, e), { glob: false })))
-    t.strictSame(entries, expected)
+    t.strictSame(
+      entries.sort((a, b) => a.localeCompare(b, 'en')),
+      expected,
+    )
     count += 1
   }
 
   t.strictSame(readdirSync(dir), [])
-  t.equal(count, totalAttempts)
+  t.equal(count, iterations)
 })
